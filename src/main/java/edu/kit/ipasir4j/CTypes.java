dbea642d3ffb966d8bc548e727b7d4a85b4a9d42
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-01-20 13:09:05 +0100

    Add private constructor to CTypes class

--- src/main/java/edu/kit/ipasir4j/CTypes.java
+++ src/main/java/edu/kit/ipasir4j/CTypes.java
@@ -1,11 +1,14 @@
 package edu.kit.ipasir4j;
 
+import java.nio.ByteOrder;
 import jdk.incubator.foreign.MemoryLayout;
 
-import java.nio.ByteOrder;
-
+@SuppressWarnings("checkstyle:AbbreviationAsWordInName")
 final class CTypes {
 
   static final MemoryLayout INT_32 = MemoryLayout.valueLayout(4, ByteOrder.nativeOrder());
 
+  private CTypes() {
+
+  }
 }
