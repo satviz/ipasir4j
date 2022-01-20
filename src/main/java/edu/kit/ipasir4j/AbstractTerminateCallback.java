e687992c1fc5ab7b301a6b789ab13b49a8ed50fd
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-01-20 13:06:45 +0100

    Document AbstractTerminateCallback

--- src/main/java/edu/kit/ipasir4j/AbstractTerminateCallback.java
+++ src/main/java/edu/kit/ipasir4j/AbstractTerminateCallback.java
@@ -2,6 +2,13 @@ package edu.kit.ipasir4j;
 
 import jdk.incubator.foreign.MemoryAddress;
 
+/**
+ * An abstraction on top of {@link SolverTerminateCallback}.<br>
+ * It transforms the data passed to it to a more convenient Java representation
+ * and enables you to return a {@code boolean} instead of an {@code int}.
+ *
+ * @param <T> The type of {@link SolverData} this callback should receive.
+ */
 public abstract class AbstractTerminateCallback<T extends SolverData>
     implements AbstractCallback<T>, SolverTerminateCallback {
 
@@ -10,5 +17,11 @@ public abstract class AbstractTerminateCallback<T extends SolverData>
     return onTerminateQuestion(dataFrom(dataAddr)) ? 1 : 0;
   }
 
+  /**
+   * Called when the solver asks whether it should terminate the solving process.
+   *
+   * @param data The {@link SolverData} object.
+   * @return {@code true} if it should be terminated, {@code false} if not.
+   */
   protected abstract boolean onTerminateQuestion(T data);
 }
