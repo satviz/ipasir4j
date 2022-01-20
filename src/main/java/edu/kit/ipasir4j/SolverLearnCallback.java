267ca72a981864a364a193f632b8f2d017163bf6
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-01-20 12:39:42 +0100

    Document learn callback

--- src/main/java/edu/kit/ipasir4j/SolverLearnCallback.java
+++ src/main/java/edu/kit/ipasir4j/SolverLearnCallback.java
@@ -2,8 +2,19 @@ package edu.kit.ipasir4j;
 
 import jdk.incubator.foreign.MemoryAddress;
 
+/**
+ * A low-level interface representing a learn callback function for ipasir.
+ */
+@FunctionalInterface
 public interface SolverLearnCallback {
 
+  /**
+   * Called when a solver learns a new clause.
+   *
+   * @param data The {@code data} pointer initially passed to
+   *             {@link Solver#setLearn(MemoryAddress, int, SolverLearnCallback)}
+   * @param clause The address of the beginning of a 0-terminated memory segment of literals (int).
+   */
   void onClauseLearn(MemoryAddress data, MemoryAddress clause);
 
 }
