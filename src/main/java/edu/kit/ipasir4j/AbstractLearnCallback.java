1e68faaa0d5cbf0e821942da1e58b5fd309f3bdd
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-01-20 13:03:27 +0100

    Document and fix checkstyle in AbstractLearnCallback

--- src/main/java/edu/kit/ipasir4j/AbstractLearnCallback.java
+++ src/main/java/edu/kit/ipasir4j/AbstractLearnCallback.java
@@ -4,18 +4,31 @@ import jdk.incubator.foreign.MemoryAccess;
 import jdk.incubator.foreign.MemoryAddress;
 import jdk.incubator.foreign.MemorySegment;
 
-public abstract class AbstractLearnCallback<T extends SolverData> implements AbstractCallback<T>, SolverLearnCallback {
+/**
+ * An abstraction on top of {@link SolverLearnCallback}.<br>
+ * It transforms the data passed to it to a more convenient Java representation.
+ *
+ * @param <T> The type of {@link SolverData} this callback should receive.
+ */
+public abstract class AbstractLearnCallback<T extends SolverData>
+    implements AbstractCallback<T>, SolverLearnCallback {
 
   @Override
   public final void onClauseLearn(MemoryAddress dataAddr, MemoryAddress clauseAddr) {
     int[] clause = MemorySegment.globalNativeSegment().asSlice(clauseAddr)
         .elements(CTypes.INT_32)
         .mapToInt(MemoryAccess::getInt)
-        .takeWhile((i) -> i != 0)
+        .takeWhile(i -> i != 0)
         .toArray();
     T data = dataFrom(dataAddr);
-    onSolverLearn(data, clause);
+    onClauseLearn(data, clause);
   }
 
-  protected abstract void onSolverLearn(T data, int[] clause);
+  /**
+   * Called when a solver learns a clause.
+   *
+   * @param data The {@link SolverData} object.
+   * @param clause The clause that was learned, represented as an array of literals.
+   */
+  protected abstract void onClauseLearn(T data, int[] clause);
 }
