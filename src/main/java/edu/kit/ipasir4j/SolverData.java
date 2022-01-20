d046acf3d506d184d9b97deccfb67bb7f137c9af
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-01-20 12:26:07 +0100

    Document SolverData

--- src/main/java/edu/kit/ipasir4j/SolverData.java
+++ src/main/java/edu/kit/ipasir4j/SolverData.java
@@ -3,6 +3,18 @@ package edu.kit.ipasir4j;
 import jdk.incubator.foreign.CLinker;
 import jdk.incubator.foreign.MemoryAddress;
 
+/**
+ * A {@code SolverData} object is the Java representation of a {@code data} pointer that is passed
+ * to {@link Solver#setTerminate(MemoryAddress, SolverTerminateCallback) ipasir_set_terminate} or
+ * {@link Solver#setLearn(MemoryAddress, int, SolverLearnCallback) ipasir_set_learn}. This pointer
+ * can be used to gain access to additional solver-specific data.
+ *
+ * <p>The definition of the structure of this data and a corresponding Java API is delegated to
+ * implementations of this class.
+ *
+ * <p>Instances of this class should be treated as resources that must be closed.
+ * What {@link #close()} does is, again, implementation-defined.
+ */
 public abstract class SolverData implements AutoCloseable {
 
   private final MemoryAddress address;
@@ -11,10 +23,20 @@ public abstract class SolverData implements AutoCloseable {
     this.address = address;
   }
 
+  /**
+   * Gets the address of the underlying data pointer.
+   *
+   * @return Return the address in memory of the data this object represents.
+   */
   public MemoryAddress getAddress() {
     return address;
   }
 
+  /**
+   * Closes this data object by freeing the memory at the underlying data pointer.
+   *
+   * <p>This is only the default behaviour and may be overwritten by implementations.
+   */
   @Override
   public void close() {
     CLinker.freeMemory(address);
