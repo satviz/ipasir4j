f03ca6b6c4318c7f1f9c9ef2883289f00411d533
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-01-20 12:41:48 +0100

    Document terminate callback

--- src/main/java/edu/kit/ipasir4j/SolverTerminateCallback.java
+++ src/main/java/edu/kit/ipasir4j/SolverTerminateCallback.java
@@ -2,8 +2,19 @@ package edu.kit.ipasir4j;
 
 import jdk.incubator.foreign.MemoryAddress;
 
+/**
+ * A low-level interface representing a terminate callback function for ipasir.
+ */
+@FunctionalInterface
 public interface SolverTerminateCallback {
 
+  /**
+   * Called by ipasir to check whether the solving process should be terminated.
+   *
+   * @param data The {@code data} pointer initially passed to
+   *             {@link Solver#setTerminate(MemoryAddress, SolverTerminateCallback)}
+   * @return a "boolean int" (1 for true, 0 for false)
+   */
   int onTerminateQuestion(MemoryAddress data);
 
 }
