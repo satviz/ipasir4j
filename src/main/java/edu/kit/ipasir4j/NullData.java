ebe63a69b9653e6cb29f5930a236d4c5cbd5d1f8
JohnnyJayJay <johnnyjayjay02@gmail.com>
2022-01-20 12:42:55 +0100

    Complete NullData docs

--- src/main/java/edu/kit/ipasir4j/NullData.java
+++ src/main/java/edu/kit/ipasir4j/NullData.java
@@ -4,9 +4,14 @@ import jdk.incubator.foreign.MemoryAddress;
 
 /**
  * An implementation of {@link SolverData} representing a null pointer, or "no data".
+ *
+ * @see #INSTANCE
  */
 public final class NullData extends SolverData {
 
+  /**
+   * The singleton instance of this class.
+   */
   public static final NullData INSTANCE = new NullData();
 
   private NullData() {
